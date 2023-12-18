ef55c2c4a6cf48f235fa48cc04bea6865d5d6ea8
Dabeer Ahmed <dabeerahmed962@gmail.com>
2023-12-18 21:54:35 +0500

    some changes in the product controller folder

--- controller/product.js
+++ controller/product.js
@@ -22,7 +22,7 @@ exports.createProduct = (req, res) => {
 
 // Read GET products
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find({ price: { $gt: 500 } });
+  const products = await Product.find({});
   res.json(products);
 };
 
